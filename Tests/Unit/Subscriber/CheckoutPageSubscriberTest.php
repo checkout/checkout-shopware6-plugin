ceddd40356c42ddbf09ed1925233a4950370b76f
ved-beejadhur-cko <ved.beejadhur@checkout.com>
2021-01-19 11:56:24 +0400

    ECPINT-1983: store logger

--- Tests/Unit/Subscriber/CheckoutPageSubscriberTest.php
+++ Tests/Unit/Subscriber/CheckoutPageSubscriberTest.php
@@ -2,7 +2,6 @@
 
 use PHPUnit\Framework\TestCase;
 use Checkoutcom\Subscriber\CheckoutPageSubscriber;
-use Checkoutcom\Helper\ckoException;
 use Checkoutcom\Tests\Unit\Common\MockTest;
 use Checkoutcom\Tests\Unit\Common\ConstantTest;
 
@@ -30,13 +29,5 @@ class CheckoutPageSubscriberTest extends TestCase
             
         $this->assertContains($value[0], ConstantTest::PAYMENT_METHOD_CATEGORY); 
         $this->assertContains($value[1], ConstantTest::PAYMENT_METHOD_CATEGORY); 
-    }
-
-    public function testExpectException() {
-
-        CheckoutPageSubscriber::getCkoContext("token_123", "wrong_pk", 'EUR');
-
-        $this->expectException(ckoException::class);
-
     }
 }
